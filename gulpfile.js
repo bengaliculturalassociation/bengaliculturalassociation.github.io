13c71412517ffc788e3cef1c5a30737843689ee9
Anubhab Sen <anubhabsen@gmail.com>
2017-03-29 02:06:48 +0530

    adds event page, logo and images ready to make grid

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,21 @@ images = ['./assets/img/index/1.jpg',
 			'./assets/img/index/6.jpg',
 ]
 
+imagesEvents = ['./assets/img/events/2.jpg',
+			'./assets/img/events/3.jpg',
+			'./assets/img/events/4.jpg',
+			'./assets/img/events/5.jpg',
+			'./assets/img/events/6.jpg',
+			'./assets/img/events/7.jpg',
+			'./assets/img/events/8.jpg',
+]
+
 gulp.task('resize', function () {
-  gulp.src(images)
+  gulp.src(imagesEvents)
     .pipe(imageResize({
-      width : 300,
-      height : 300,
-      crop : true,
+      width : 500,
+      height : 500,
+      crop : false,
       upscale : true
     }))
     .pipe(gulp.dest('gulpimages'));
